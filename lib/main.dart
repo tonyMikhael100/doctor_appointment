83ca999e201486a86e8c833b3186f24fb7e69e06
tonyMikhael100 <tonyMikhael100@gmail.com>
2025-08-12 23:09:57 +0300

    light theme
    Signed-off-by: tonyMikhael100 <tonyMikhael100@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter  course',
+      title: 'Flutter advanced test ',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
