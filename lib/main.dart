a8bbb3e0b6fadeb3c01c9df4e761a3d7c0b33a9c
tonyMikhael100 <tonyMikhael100@gmail.com>
2025-08-17 15:58:51 +0300

    add splash screen features for android above 12 only and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:doctor_appointment/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
   runApp(
     const DocApp(),
   );
