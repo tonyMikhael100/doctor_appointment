2cee8e3dfdd1ec8bd2884f33ab5a33f7db591017
tonyMikhael100 <tonyMikhael100@gmail.com>
2025-08-19 19:14:54 +0300

    fake commit for splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:doctor_appointment/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() async {
+void main() {
   runApp(
     const DocApp(),
   );
